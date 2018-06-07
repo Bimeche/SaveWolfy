7f7b47c79b8e2bf5b161e820bf08d243471ddef8
Bimeche <leo.gar@hotmail.fr>
2018-06-07 23:32:33 +0200

    Cow are spawning at regular interval faster and faster

--- SaveWolfy/Assets/Scripts/CowSpawner.cs
+++ SaveWolfy/Assets/Scripts/CowSpawner.cs
@@ -3,16 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CowSpawner : MonoBehaviour {
-	public Transform cow;
-	public Transform spawner;
+
+	public GameObject cow;
+	public Rect spawnZone;
+	public float minSpawnForce = 1f;
+	public float maxSpawnForce = 1000f;
+	public float baseSpawnTime = 3f;
+	public float regularTimeReduction = 0.01f;
+	public float minimalSpawnTime = 0.5f;
+
+	private GameManager gMan;
+
 	// Use this for initialization
 	void Start () {
-		Invoke("SpawnCow", 2f);
+		gMan = FindObjectOfType<GameManager>();
+		Invoke("RegularCowSpawn", baseSpawnTime);
+		Debug.Log(spawnZone.xMin + "  " + spawnZone.xMax + "  " + spawnZone.yMin + "  " + spawnZone.yMax);
 	}
 
-	void SpawnCow () {
-		Transform cowSpawned = Instantiate(cow, spawner.position, Quaternion.identity);
-		cowSpawned.GetComponent<Rigidbody2D>().AddForce(Vector2.up * Random.Range(300f, 700f));
-		Invoke("SpawnCow", Random.Range(6f, 10f));
+	void RegularCowSpawn () {
+		Vector2 spawnPosition = new Vector2(Random.Range(spawnZone.xMin,spawnZone.xMax), Random.Range(spawnZone.yMin, spawnZone.yMax));
+
+		if(!IsSpawnPossible(spawnPosition, cow.GetComponent<CircleCollider2D>().radius))
+		{
+			Debug.Log("cow collided at spawn, not spawned");
+			baseSpawnTime -= regularTimeReduction;
+			if (baseSpawnTime < minimalSpawnTime)
+				baseSpawnTime = minimalSpawnTime;
+			Invoke("RegularCowSpawn", baseSpawnTime);
+			return;
+		}
+
+		GameObject cowSpawned = Instantiate(cow, spawnPosition, Quaternion.identity);
+
+		Vector2 angle;
+		if (spawnPosition.x < 0)
+			angle = new Vector2(Random.Range(-spawnPosition.x / 3, -spawnPosition.x), -Random.Range(3f, 7f));
+		else if (spawnPosition.x > 0)
+			angle = new Vector2(Random.Range(-spawnPosition.x, -spawnPosition.x / 3), -Random.Range(3f, 7f));
+		else
+			angle = new Vector2(Random.Range(-6f, 6f), -Random.Range(3f, 7f));
+
+
+		cowSpawned.GetComponent<Rigidbody2D>().AddForce(angle * Random.Range(minSpawnForce, maxSpawnForce));
+		gMan.cowsSpawned.Add(cowSpawned.transform);
+		baseSpawnTime -= regularTimeReduction;
+		if (baseSpawnTime < minimalSpawnTime)
+			baseSpawnTime = minimalSpawnTime;
+		Invoke("RegularCowSpawn", baseSpawnTime);
+	}
+
+	bool IsSpawnPossible (Vector2 targetPosition, float checkRadius) {
+		var checkResult = Physics2D.OverlapCircle(targetPosition, checkRadius);
+		return checkResult == null;
 	}
 }
--- SaveWolfy/Assets/Scripts/GameManager.cs
+++ SaveWolfy/Assets/Scripts/GameManager.cs
@@ -4,24 +4,19 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
-	public Transform cow;
 	public RectTransform pausePanel;
 	public RectTransform pauseButtonPanel;
 	public RectTransform currentScorePanel;
 	public RectTransform endGamePanel;
-	public float spawnForce = 100f;
 	public Text scoreText;
 	public Text bestScore;
 	public Text finalScore;
 	public Text scoreToBeat;
 	private int playerScore = 0;
 	private float scoreUpdate = 0.2f;
-	private int numberOfCowsSpawned = 0;
-	public float spawnTime = 2f;
-	private GameObject[] spawns;
-	private List<Transform> cowsSpawned;
+	[HideInInspector]
+	public List<Transform> cowsSpawned;
 	private bool paused = false;
-	int palier = 5;
 	int comboCount = 0;
 	private bool gameEnded;
 
@@ -32,72 +27,16 @@ public class GameManager : MonoBehaviour {
 		Time.timeScale = 1f;
 		gameEnded = false;
 		cowsSpawned = new List<Transform>();
-		spawns = GameObject.FindGameObjectsWithTag("CowSpawn");
 		pausePanel.GetComponent<CanvasGroup>().alpha = 0;
 		pausePanel.GetComponent<CanvasGroup>().blocksRaycasts = false;
 		endGamePanel.GetComponent<CanvasGroup>().alpha = 0;
 		endGamePanel.GetComponent<CanvasGroup>().blocksRaycasts = false;
-		Invoke("SpawnCow", spawnTime);
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.P))
 			PauseGame();
-	}
-
-	private void SpawnCow () {
-		bool cowSpawned = false;
-		List<int> intList = new List<int>();
-		for (int h = 0; h < 7; h++) {
-			intList.Add (h);
-		}
-		for (int i = 0; i < palier; i++) {//pallier part à 5
-			intList.RemoveAt (Random.Range (0, (intList.Count)));
-		}
-		foreach (int j in intList) {
-			int rand = Random.Range (0, 2);
-			if (rand == 1) {
-				GameObject spawnPoint = spawns [j];
-				Transform temp = Instantiate(cow, spawnPoint.transform.position, Quaternion.identity);
-				cowsSpawned.Add(temp);
-				spawnForce = Random.Range(50f, 200f);
-				Vector2 angle;
-				if (spawnPoint.transform.position.x < 0)
-					angle = new Vector2(Random.Range(-spawnPoint.transform.position.x / 3, -spawnPoint.transform.position.x), -Random.Range(3f, 7f));
-				else if (spawnPoint.transform.position.x > 0)
-					angle = new Vector2(Random.Range(-spawnPoint.transform.position.x, -spawnPoint.transform.position.x / 3), -Random.Range(3f, 7f));
-				else
-					angle = new Vector2(Random.Range(-6f, 6f), -Random.Range(3f, 7f));
-				temp.GetComponent<Rigidbody2D>().AddForce(angle * spawnForce);
-				numberOfCowsSpawned++;
-				cowSpawned = true;
-			}
-		}
-
-		// si aucune vache n'a spawn on en spawn une sur un spawner choisit au hasard
-		if (!cowSpawned)
-		{
-			GameObject spawnPoint = spawns[Random.Range(0,7)];
-			Transform temp = Instantiate(cow, spawnPoint.transform.position, Quaternion.identity);
-			cowsSpawned.Add(temp);
-			spawnForce = Random.Range(50f, 200f);
-			Vector2 angle;
-			if (spawnPoint.transform.position.x < 0)
-				angle = new Vector2(Random.Range(-spawnPoint.transform.position.x / 3, -spawnPoint.transform.position.x), -Random.Range(3f, 7f));
-			else if (spawnPoint.transform.position.x > 0)
-				angle = new Vector2(Random.Range(-spawnPoint.transform.position.x, -spawnPoint.transform.position.x / 3), -Random.Range(3f, 7f));
-			else
-				angle = new Vector2(Random.Range(-6f, 6f), -Random.Range(3f, 7f));
-			temp.GetComponent<Rigidbody2D>().AddForce(angle * spawnForce);
-			numberOfCowsSpawned++;
-		}
-
-		if (numberOfCowsSpawned >= 15 && palier >= 0) {
-			palier = palier - 1;
-			numberOfCowsSpawned = 0;
-		}
-		Invoke("SpawnCow", spawnTime);
 	}
 
 	public void DestroyCow (GameObject go, int strikeMeter) {
