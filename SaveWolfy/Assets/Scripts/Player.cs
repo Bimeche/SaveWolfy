dc2af4b0ca0f87266c44f860cd18fd4ebd3820f9
Norwhel <paul.led@hotmail.fr>
2018-06-07 16:27:22 +0200

    adding new script for moving behavior with clic and trail. sorry i changed a bit sceneLeo --'

--- SaveWolfy/Assets/Scripts/Player.cs
+++ SaveWolfy/Assets/Scripts/Player.cs
@@ -4,19 +4,63 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 	Camera cam;
+	bool isKicking = false;
+	Rigidbody2D rb;
+	CircleCollider2D circleCollider;
+	public GameObject footTrailPrefab;
+	private GameObject currentFootTrail;
+
 	// Use this for initialization
 	void Start () {
 		cam = Camera.main;
+		rb = GetComponent<Rigidbody2D> ();
+		circleCollider = GetComponent<CircleCollider2D> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		Vector3 pos = Input.mousePosition;
+		if (Input.GetMouseButtonDown (0)) {
+			StartKicking ();
+		} else if (Input.GetMouseButtonUp (0)) {
+			StopKicking ();
+		}
+
+		if (isKicking) {
+			UpdateKick ();
+		}
+
+		//////////////
+		/*Touch[] touches = Input.touches;
+		if(touches.Length > 0){
+			Vector3 pos = new Vector3(touches[0].position.x, touches[0].position.y, touches[0].position.z);
+			paddle.transform.position = pos;*/
+		/////////////
+
+		/*Vector3 pos = Input.mousePosition;
 		pos.z = transform.position.z - Camera.main.transform.position.z;
-		Move(pos);
+		Move(pos);*/
 	}
 
-	void Move (Vector3 position) {
+	void UpdateKick(){
+		Vector2 newPosition = cam.ScreenToWorldPoint (Input.mousePosition);
+		rb.position = newPosition;
+		}
+	
+	void StartKicking (){
+		isKicking = true;
+		currentFootTrail = Instantiate (footTrailPrefab, transform);
+		//previousPosition = cam.ScreenToWorldPoint (Input.mousePosition);
+		circleCollider.enabled = true;
+	}
+
+	void StopKicking(){
+		isKicking = false;
+		currentFootTrail.transform.SetParent (null);
+		Destroy (currentFootTrail, 2f);
+		circleCollider.enabled = false;
+	}
+
+	/*void Move (Vector3 position) {
 		//Restricting position for the mouse
 		if (position.x > Screen.width)
 			position.x = Screen.width;
@@ -29,6 +73,6 @@ public class Player : MonoBehaviour {
 			position.y = 0;
 
 		transform.position = cam.ScreenToWorldPoint(position);
-	}
+	}*/
 }
 
