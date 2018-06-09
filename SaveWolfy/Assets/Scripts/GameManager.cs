2dcc3b52203e536a94a1c55b09146d26840f4ee6
Bimeche <leo.gar@hotmail.fr>
2018-06-09 18:30:15 +0200

    Ratio working now

--- SaveWolfy/Assets/Scripts/GameManager.cs
+++ SaveWolfy/Assets/Scripts/GameManager.cs
@@ -19,8 +19,49 @@ public class GameManager : MonoBehaviour {
 	private bool paused = false;
 	int comboCount = 0;
 	private bool gameEnded;
+	public float aspectWidth = 16.0f;
+	public float aspectHeight = 9.0f;
+
+	void adaptScreenRatio () {
+
+		// set the desired aspect ratio (the values in this example are
+		// hard-coded for 16:9, but you could make them into public
+		// variables instead so you can set them at design time)
+		float targetAspect = aspectWidth / aspectHeight;
+
+		// determine the game window's current aspect ratio
+		float windowAspect = (float)Screen.width / Screen.height;
+
+		// current viewport height should be scaled by this amount
+		float scaleHeight = windowAspect / targetAspect;
 
 
+		// if scaled height is less than current height, add letterbox
+		if (scaleHeight < 1.0f)
+		{
+			Rect rect = Camera.main.rect;
+
+			rect.width = 1.0f;
+			rect.height = scaleHeight;
+			rect.x = 0;
+			rect.y = (1.0f - scaleHeight) / 2.0f;
+
+			Camera.main.rect = rect;
+		}
+		else // add pillarbox
+		{
+			float scaleWidth = 1.0f / scaleHeight;
+
+			Rect rect = Camera.main.rect;
+
+			rect.width = scaleWidth;
+			rect.height = 1.0f;
+			rect.x = (1.0f - scaleWidth) / 2.0f;
+			rect.y = 0;
+
+			Camera.main.rect = rect;
+		}
+	}
 	// Use this for initialization
 	void Start () {
 		Cursor.visible = true;
@@ -31,6 +72,7 @@ public class GameManager : MonoBehaviour {
 		pausePanel.GetComponent<CanvasGroup>().blocksRaycasts = false;
 		endGamePanel.GetComponent<CanvasGroup>().alpha = 0;
 		endGamePanel.GetComponent<CanvasGroup>().blocksRaycasts = false;
+		adaptScreenRatio();
 	}
 
 	// Update is called once per frame
