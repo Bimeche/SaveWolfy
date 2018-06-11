4a7f003c8ddb3b3b5928d3ba7a8a17b357ce14d2
Norwhel <paul.led@hotmail.fr>
2018-06-11 17:54:06 +0200

    continued work on fx and UX

--- SaveWolfy/Assets/Scripts/CowScript.cs
+++ SaveWolfy/Assets/Scripts/CowScript.cs
@@ -7,6 +7,15 @@ public class CowScript : AIManager {
 	public int strikeMeter = 0;
 	public bool comboTouch = false;
 	private Animator anim;
+	public Transform fxDeath;
+	public Transform fxHit;
+	public Transform fxTrail;
+	public Transform fxstrikeMeter1;
+	public Transform fxstrikeMeter2;
+	public Transform fxstrikeMeter3;
+	public Transform fxstrikeMeter4;
+	public Transform fxstrikeMeter5;
+	bool panic = false;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +26,7 @@ public class CowScript : AIManager {
 
 	// Update is called once per frame
 	void FixedUpdate () {
+		StrikedState (strikeMeter);
 		anim.SetBool ("Striked", false);
 		anim.SetFloat ("Altitude", transform.position.y);
 		if (rb.velocity.magnitude > maxMagnitude)
@@ -34,6 +44,10 @@ public class CowScript : AIManager {
 				Vector2 impactSpeed = new Vector2 (collision.contacts [0].relativeVelocity.x - rb.velocity.x, collision.contacts [0].relativeVelocity.y - rb.velocity.y);
 
 				impactPoint = -impactPoint.normalized;
+
+				if (fxHit) {
+					Destroy (Instantiate (fxHit, collision.contacts [0].point, Quaternion.Euler (new Vector3 (0, 0, -1))).gameObject, 0.5f);
+				}
 
 				float magnitude = Mathf.Sqrt (impactSpeed.magnitude) * playerForce;
 				if (magnitude < minMagnitude)
@@ -55,10 +69,49 @@ public class CowScript : AIManager {
 		}
 	}
 
+	private void StrikedState(int striked){
+		switch (striked) {
+		case 0:
+			GetComponent<SpriteRenderer> ().color = new Color (1f, 1f, 1f);
+			break;
+		case 1:
+			GetComponent<SpriteRenderer> ().color = new Color (1f, 0.9f, 0.9f);
+			break;
+		case 2:
+			GetComponent<SpriteRenderer> ().color = new Color (1f, 0.8f, 0.8f);
+			break;
+		case 3:
+			GetComponent<SpriteRenderer> ().color = new Color (1f, 0.7f, 0.7f);
+			break;
+		case 4:
+			GetComponent<SpriteRenderer> ().color = new Color (1f, 0.6f, 0.6f);
+			break;
+		case 5:
+			GetComponent<SpriteRenderer> ().color = new Color (1f, 0.5f, 0.5f);
+			break;
+		case 6:
+			GetComponent<SpriteRenderer> ().color = new Color (1f, 0.4f, 0.4f);
+			break;
+		case 7:
+			GetComponent<SpriteRenderer> ().color = new Color (1f, 0.3f, 0.3f);
+			break;
+		case 8:
+			GetComponent<SpriteRenderer> ().color = new Color (1f, 0.2f, 0.2f);
+			break;
+		case 9:
+			GetComponent<SpriteRenderer> ().color = new Color (1f, 0.1f, 0.1f);
+			break;
+		case 10:
+			GetComponent<SpriteRenderer> ().color = new Color (1f, 0f, 0f);
+			break;
+		}
+	}
+
 	private void OnBecameVisible () {
 		isCowVisible = true;
 	}
 
 	public void OnDestroy(){
+		Destroy (Instantiate (fxDeath, transform.position, Quaternion.identity).gameObject, 1f);
 	}
 }
